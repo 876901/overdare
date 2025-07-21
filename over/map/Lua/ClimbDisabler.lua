9a59166199e14255b7eca071ae49b4e9ea06acb0
876901 <127916380+876901@users.noreply.github.com>
2025-07-21 19:51:56 +0900

    map 가져옴
    맵 가져왔는데 동규가 한거 가려졌을 수 도 ???

---
+++ over/map/Lua/ClimbDisabler.lua
@@ -0,0 +1,6 @@
+﻿local Character = script.Parent
+
+repeat wait(0.1) until Character.Humanoid
+local Humanoid = Character.Humanoid
+
+Humanoid:SetStateEnabled(Enum.HumanoidStateType.Climbing, false)
